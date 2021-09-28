0bf3dff91aab16e4fdad9e73399e8f4b3428854d
Irena Chen <irena1009@gmail.com>
2021-09-28 11:47:49 +0800

    finish topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./topbar.css"
-import { NotificationsNone } from '@material-ui/icons';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 export default function Topbar() {
     return (
@@ -14,6 +14,13 @@ export default function Topbar() {
                         <NotificationsNone />
                         <span className="topIconBadge">2</span>
                     </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
                 </div>
             </div>
         </div>
