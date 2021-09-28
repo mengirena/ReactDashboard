8664d1057bb23893ac39f1a2efc41335b351a3b9
Irena Chen <irena1009@gmail.com>
2021-09-28 10:03:11 +0800

    topbar modification

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./topbar.css"
+import { NotificationsNone } from '@material-ui/icons';
 
 export default function Topbar() {
     return (
-        <div>
-            topbar
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topbarLeft">
+                    <div className="logo">SHC</div>
+                </div>
+                <div className="topbarRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
