940604d592cba517233f9fbb039917a284f52a1f
Irena Chen <irena1009@gmail.com>
2021-10-01 08:40:05 +0800

    create dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        user: 4000,
+    },
+    {
+        name: 'Feb',
+        user: 3000,
+    },
+    {
+        name: 'Mar',
+        user: 2000,
+    },
+    {
+        name: 'Apr',
+        user: 2780,
+    },
+    {
+        name: 'May',
+        user: 1890,
+    },
+    {
+        name: 'Jun',
+        user: 2390,
+    },
+    {
+        name: 'July',
+        user: 3490,
+    },
+    {
+        name: 'Aug',
+        user: 4000,
+    },
+    {
+        name: 'Sep',
+        user: 3000,
+    },
+    {
+        name: 'Oct',
+        user: 2000,
+    },
+    {
+        name: 'Nov',
+        user: 2780,
+    },
+    {
+        name: 'Dec',
+        user: 1890,
+    },
+  ];
