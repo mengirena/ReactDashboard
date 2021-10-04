7d80e7f06814850489c24a16093fc30ef873967e
Irena Chen <irena1009@gmail.com>
2021-10-04 13:30:16 +0800

    link side bar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import "./sidebar.css"
 import { LineStyle, Timeline, TrendingUp } from "@material-ui/icons"
+import { Link } from "react-router-dom"
 
 export default function Sidebar() {
     return (
@@ -8,18 +9,24 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <LineStyle className="sidebarIcon"/>
-                            Home
-                        </li>
-                        <li className="sidebarListItem">
-                            <Timeline className="sidebarIcon"/>
-                            Analytics
-                        </li>
-                        <li className="sidebarListItem">
-                            <TrendingUp className="sidebarIcon"/>
-                            Sales
-                        </li>
+                        <Link to="/users" className="link">
+                            <li className="sidebarListItem">
+                                <LineStyle className="sidebarIcon"/>
+                                User
+                            </li>
+                        </Link>
+                        <Link to="/products" className="link">
+                            <li className="sidebarListItem">
+                                <Timeline className="sidebarIcon"/>
+                                Programs
+                            </li>
+                        </Link>
+                        <Link to="/newUser" className="link">
+                            <li className="sidebarListItem">
+                                <TrendingUp className="sidebarIcon"/>
+                                創建用戶
+                            </li>
+                        </Link>
                     </ul>
                 </div>
                 <div className="sidebarMenu">
